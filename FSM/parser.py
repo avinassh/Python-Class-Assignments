07bd3a5212285e86264255c1c5a257166f761fb8
avinassh <avinashsajjan@gmail.com>
2013-11-18 15:26:16 +0530

    list of finite objects

--- FSM/parser.py
+++ FSM/parser.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 
 fo = open('input.txt', 'r')
-for line in fo:
-	print line.split()
+#for line in fo:
+	#print line.split()
 
+class FiniteState:
+    """Class for the finite states"""
+    def __init__(self, name):
+    	self.isItInitial = False
+    	self.isItFinal = False
+    	self.name = name
 
-	
+names = ['1','2','3','4']
+
+state = []
+
+for item in names:
+	state.append(FiniteState(item))
+
+print state[0].name	
