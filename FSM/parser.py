3fef81b75b79e39890db417c5935dfc59b9a3127
avinassh <avinashsajjan@gmail.com>
2013-11-18 16:43:10 +0530

    minor modifications, now supports comments, serializes the input for better use and builds the dictionary

--- FSM/parser.py
+++ FSM/parser.py
@@ -1,6 +1,34 @@
 #!/usr/bin/python
 
-fo = open('input.txt', 'r')
+#Assumptions only one final state. Given an initial state
+#State names are always specified in numbers
+#
+#
+#
+#
+
+def lexicalAnalyzer():
+	return None  #check all invalid keywords 
+
+def serializeInput():
+	inputData = {} #is a dictionary and has all the important data
+	listOfTransitions = []
+	#inputData['transition'] = []
+	fn = open('input.txt', 'r')
+	for line in fn:
+		key = line.split()[0]
+		if (key[0] == '#'):
+			#print 'comments found'
+			continue
+		if (key == 'transition'):
+			listOfTransitions.append(line.split()[2:])
+		inputData[key] = line.split()[2:]
+	inputData['transition'] = listOfTransitions	
+	print inputData	
+	#print listOfTransitions
+
+serializeInput()			
+#fo = open('input.txt', 'r')
 #for line in fo:
 	#print line.split()
 
@@ -10,12 +38,32 @@ class FiniteState:
     	self.isItInitial = False
     	self.isItFinal = False
     	self.name = name
+    	self.validTransitions = []
+
+    def initTransitions(self, inputString, nextState):
+    	self.validTransitions.append((inputString, nextState))
+
+    def transition(self, inputString):
+    	for validTrans in self.validTransitions:
+    		if validTrans[0] == inputString:
+    			return validTrans[1]
+    	return 'DeadState:-('		
+
 
 names = ['1','2','3','4']
 
 state = []
 
-for item in names:
-	state.append(FiniteState(item))
+#for item in names:
+#	state.append(FiniteState(item))
 
-print state[0].name	
+#state[0].initTransitions('a', '2')
+#state[0].initTransitions('b', '3')
+
+#print state[0].transition('a')
+#print state[0].transition('b')
+
+#initial = state[0]
+
+#print state[0]
+#print initial
