41d2f91ed3b673bb4d73f165bc6ab922ecf3eae0
avinassh <avinashsajjan@gmail.com>
2013-11-24 21:03:12 +0530

    not working last level to generate raw_subtrees and added lots of REPL

--- Tree/test.py
+++ Tree/test.py
@@ -67,6 +67,7 @@ class node(object):
 
 def create_child_nodes(root, file_data):
     end  = 0
+    #print file_data, '\n'
     for index, char in enumerate(file_data):
         if index < end:
             #print 'here1', index
@@ -75,6 +76,7 @@ def create_child_nodes(root, file_data):
             root.val = file_data[index+1]
         if char == "subtree:":
             child = node()
+            all_nodes.append(child)
             start, end = get_subtree_data(file_data, index)
             child.raw_subtrees = file_data[start:end]
             child.parent = root
@@ -90,10 +92,11 @@ def create_child_nodes(root, file_data):
 #print root1.subtrees[1].subtrees[1].raw_subtrees
 
 def tree_build(node, file_data):
-    if len(node.subtrees):
+    if len(node.subtrees):# == 0:
+        print 'here'#file_data
         for child in node.subtrees:
-            tree_build(child, child.raw_subtrees)
-    create_child_nodes(node, file_data)        
+            tree_build(child, child.raw_subtrees)                
+    create_child_nodes(node, node.raw_subtrees)
 
 fn = open('tree_input.txt', 'r')
 full_file = fn.read()
@@ -101,7 +104,10 @@ fn.close()
 
 file_data = full_file.split()
 
+all_nodes = []
+
 root1 = node()
+all_nodes.append(root1)
 root1.raw_subtrees = file_data[1:-1]
 root1.val = '1'
 
@@ -110,11 +116,54 @@ create_child_nodes(root1, root1.raw_subtrees)
 for child in root1.subtrees:
     tree_build(child, child.raw_subtrees)
 
-#print root1.subtrees
-#print root1.raw_subtrees
-#print root1.subtrees[1].subtrees
-print root1.subtrees[1].subtrees[0].raw_subtrees
-print root1.subtrees[1].subtrees[1].raw_subtrees
+#----------------------------------------------
+# REPL : List of subtrees of a node [as lits of objects]
+#----------------------------------------------
+# print root1.subtrees
 
-print root1.subtrees[1].subtrees[0].val
-print root1.subtrees[1].subtrees[1].val
+# print root1.subtrees[0].subtrees
+# print root1.subtrees[1].subtrees
+# print root1.subtrees[2].subtrees
+# print root1.subtrees[3].subtrees
+# print root1.subtrees[4].subtrees
+
+# print root1.subtrees[0].subtrees[0].subtrees
+
+# print root1.subtrees[1].subtrees[0].subtrees
+# print root1.subtrees[1].subtrees[1].subtrees
+
+
+#----------------------------------------------
+# REPL : raw subtree data of a node
+#----------------------------------------------
+# print root1.raw_subtrees
+
+# print root1.subtrees[0].raw_subtrees
+# print root1.subtrees[1].raw_subtrees
+# print root1.subtrees[2].raw_subtrees
+# print root1.subtrees[3].raw_subtrees
+# print root1.subtrees[4].raw_subtrees
+
+# print root1.subtrees[0].subtrees[0].raw_subtrees
+
+# print root1.subtrees[1].subtrees[0].raw_subtrees
+# print root1.subtrees[1].subtrees[1].raw_subtrees
+
+
+#----------------------------------------------
+# REPL : value of a node
+#----------------------------------------------
+#print root1.val
+#print root1.subtrees[0].val
+#print root1.subtrees[1].val
+#print root1.subtrees[2].val
+#print root1.subtrees[3].val
+#print root1.subtrees[4].val
+
+#print root1.subtrees[0].subtrees[0].val
+
+#print root1.subtrees[1].subtrees[0].val
+#print root1.subtrees[1].subtrees[1].val
+
+#print root1.subtrees[2].subtrees[0].val
+#print root1.subtrees[2].subtrees[1].val
